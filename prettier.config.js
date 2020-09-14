fc3a56ad8215bf03aa0d0c253bd5b6fcee988513
Andrii Serputko <andrii.serputko@sap.com>
2020-09-14 12:33:28 +0300

    Lesson 1

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    arrowParens: 'always',
+};
